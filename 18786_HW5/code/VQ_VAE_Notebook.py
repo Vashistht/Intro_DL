e0c6f72c28a8391e9c1c88b66781f1ff34094524
Vashisth <vtiwari2@u.rochester.edu>
2024-04-07 22:57:21 -0400

    add vq vae without comit loss

---
+++ 18786_HW5/code/VQ_VAE_Notebook.py
@@ -0,0 +1,398 @@
+# %%
+# !pip install umap-learn
+
+# %%
+from __future__ import print_function
+
+# export MPLCONFIGDIR=~/.config/matplotlib
+import os
+os.environ['MPLCONFIGDIR'] = os.path.expanduser('~/.config/matplotlib')
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import savgol_filter
+import importlib_metadata as metadata
+
+from six.moves import xrange
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torch.optim as optim
+
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torchvision.utils import make_grid
+
+# %%
+# Use this if you are using any Cuda enabled system
+if torch.cuda.is_available():
+    device = 'cuda'
+# elif torch.backends.mps.is_available():
+#     device = 'mps' 
+else:
+    device = 'cpu'
+print(device)
+
+# %% [markdown]
+# ## Load Data
+
+# %%
+training_data = datasets.CIFAR10(root="data", train=True, download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+                                  ]))
+
+validation_data = datasets.CIFAR10(root="data", train=False, download=True,
+                                  transform=transforms.Compose([
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+                                  ]))
+
+# %%
+data_variance = np.var(training_data.data / 255.0)
+print(data_variance)
+
+# %% [markdown]
+# ## Vector Quantizer Layer
+
+# %%
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost, use_commitment_loss = True):
+        super(VectorQuantizer, self).__init__()
+
+        self._embedding_dim = embedding_dim
+        self._num_embeddings = num_embeddings
+
+        self._embedding = nn.Embedding(self._num_embeddings, self._embedding_dim)
+        self._embedding.weight.data.uniform_(-1/self._num_embeddings, 1/self._num_embeddings)
+        self._commitment_cost = commitment_cost
+
+    def forward(self, inputs):
+        # convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+
+        # Flatten input
+        flat_input = inputs.view(-1, self._embedding_dim)
+
+        # Calculate distances
+        distances = (torch.sum(flat_input**2, dim=1, keepdim=True)
+                    + torch.sum(self._embedding.weight**2, dim=1)
+                    - 2 * torch.matmul(flat_input, self._embedding.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self._num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self._embedding.weight).view(input_shape)
+
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        q_latent_loss = F.mse_loss(quantized, inputs.detach())
+        loss = q_latent_loss + self._commitment_cost * e_latent_loss
+
+        if self._use_commitment_loss:
+            loss = q_latent_loss + self._commitment_cost * e_latent_loss
+        else:
+            loss = q_latent_loss
+            
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), perplexity, encodings
+
+# %% [markdown]
+# ## Encoder & Decoder Architecture
+
+# %%
+class Residual(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_hiddens):
+        super(Residual, self).__init__()
+        self._block = nn.Sequential(
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=in_channels,
+                      out_channels=num_residual_hiddens,
+                      kernel_size=3, stride=1, padding=1, bias=False),
+            nn.ReLU(True),
+            nn.Conv2d(in_channels=num_residual_hiddens,
+                      out_channels=num_hiddens,
+                      kernel_size=1, stride=1, bias=False)
+        )
+
+    def forward(self, x):
+        return x + self._block(x)
+
+
+class ResidualStack(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(ResidualStack, self).__init__()
+        self._num_residual_layers = num_residual_layers
+        self._layers = nn.ModuleList([Residual(in_channels, num_hiddens, num_residual_hiddens)
+                             for _ in range(self._num_residual_layers)])
+
+    def forward(self, x):
+        for i in range(self._num_residual_layers):
+            x = self._layers[i](x)
+        return F.relu(x)
+
+# %%
+class Encoder(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Encoder, self).__init__()
+
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens//2,
+                                 kernel_size=4,
+                                 stride=2, padding=1)
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
+                                 out_channels=num_hiddens,
+                                 kernel_size=4,
+                                 stride=2, padding=1)
+        self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1, padding=1)
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        x = F.relu(x)
+
+        x = self._conv_2(x)
+        x = F.relu(x)
+
+        x = self._conv_3(x)
+        return self._residual_stack(x)
+
+# %%
+class Decoder(nn.Module):
+    def __init__(self, in_channels, out_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Decoder, self).__init__()
+
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1, padding=1)
+
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
+        self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens,
+                                                out_channels=num_hiddens//2,
+                                                kernel_size=4,
+                                                stride=2, padding=1)
+
+        self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens//2,
+                                                out_channels=out_channels,
+                                                kernel_size=4,
+                                                stride=2, padding=1)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+
+        x = self._residual_stack(x)
+
+        x = self._conv_trans_1(x)
+        x = F.relu(x)
+
+        return self._conv_trans_2(x)
+
+# %% [markdown]
+# ## Train
+
+# %%
+batch_size = 256
+num_training_updates = 5000
+
+num_hiddens = 128
+num_residual_hiddens = 32
+num_residual_layers = 2
+
+embedding_dim = 64
+num_embeddings = 512
+
+commitment_cost = 0.25
+
+learning_rate = 1e-3
+
+# %%
+training_loader = DataLoader(training_data,
+                             batch_size=batch_size,
+                             shuffle=True,
+                             pin_memory=True)
+
+# %%
+validation_loader = DataLoader(validation_data,
+                               batch_size=32,
+                               shuffle=True,
+                               pin_memory=True)
+
+# %%
+class Model(nn.Module):
+    def __init__(self, num_hiddens, num_residual_layers, num_residual_hiddens,
+                 num_embeddings, embedding_dim, commitment_cost, use_commitment_loss=True):
+        super(Model, self).__init__()
+
+        self._encoder = Encoder(3, num_hiddens,
+                                num_residual_layers,
+                                num_residual_hiddens)
+        self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens,
+                                      out_channels=embedding_dim,
+                                      kernel_size=1,
+                                      stride=1)
+
+        self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim,
+                                           commitment_cost, use_commitment_loss = use_commitment_loss)
+        
+        self._decoder = Decoder(embedding_dim, 3,
+                                num_hiddens,
+                                num_residual_layers,
+                                num_residual_hiddens)
+
+    def forward(self, x):
+        z = self._encoder(x)
+        z = self._pre_vq_conv(z)
+        loss, quantized, perplexity, _ = self._vq_vae(z)
+        x_recon = self._decoder(quantized)
+
+        return loss, x_recon, perplexity
+
+# %%
+model = Model(num_hiddens, num_residual_layers, num_residual_hiddens,
+              num_embeddings, embedding_dim,
+              commitment_cost, use_commitment_loss=False).to(device)
+
+# %%
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, amsgrad=False)
+
+# %%
+model.train()
+train_res_recon_error = []
+
+for i in xrange(num_training_updates):
+    (data, _) = next(iter(training_loader))
+    data = data.to(device)
+    optimizer.zero_grad()
+
+    vq_loss, data_recon, perplexity = model(data)
+    recon_error = F.mse_loss(data_recon, data) / data_variance
+    loss = recon_error + vq_loss
+    loss.backward()
+
+    optimizer.step()
+
+    train_res_recon_error.append(recon_error.item())
+
+    if (i+1) % 500 == 0:
+        print('%d iterations' % (i+1))
+        print('recon_error: %.3f' % np.mean(train_res_recon_error[-100:]))
+        print()
+
+# %% [markdown]
+# ## Plot Loss
+
+# %%
+train_res_recon_error_smooth = savgol_filter(train_res_recon_error, 201, 7)
+# Save the smoothed data
+np.save('train_res_recon_error_smooth.npy', train_res_recon_error_smooth)
+# train_res_recon_error_smooth = np.load('train_res_recon_error_smooth.npy')
+# %%
+plt.figure(figsize=(12,6))
+plt.plot(train_res_recon_error_smooth)
+plt.yscale('log')
+plt.title('Smoothed NMSE.')
+plt.xlabel('iteration')
+plt.savefig('nmse.png')
+
+# %% [markdown]
+# ## View Reconstructions
+
+# %%
+model.eval()
+
+(valid_originals, _) = next(iter(validation_loader))
+valid_originals = valid_originals.to(device)
+
+vq_output_eval = model._pre_vq_conv(model._encoder(valid_originals))
+_, valid_quantize, _, _ = model._vq_vae(vq_output_eval)
+valid_reconstructions = model._decoder(valid_quantize)
+
+# %%
+torch.max(model._encoder(valid_originals))
+
+# %%
+(train_originals, _) = next(iter(training_loader))
+train_originals = train_originals.to(device)
+_, train_reconstructions, _, _ = model._vq_vae(train_originals)
+
+torch.save(valid_reconstructions, 'valid_reconstructions.pt')
+torch.save(valid_originals, 'valid_originals.pt')
+
+
+# # %%
+# def show(img):
+#     npimg = img.numpy()
+#     fig = plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
+#     fig.axes.get_xaxis().set_visible(False)
+#     fig.axes.get_yaxis().set_visible(False)
+
+def save_new(img, file_path):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)), interpolation='nearest')
+    plt.axis('off')  # Hide axes
+    plt.savefig(file_path, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+# # %%
+# image_name = 'img'
+# show(make_grid(valid_reconstructions.cpu().data)+0.5, )
+
+# # %%
+# image_name= 'img1'
+
+# show(make_grid(valid_originals.cpu()+0.5))
+
+# # %% [markdown]
+# # ## View Embedding
+
+valid_reconstructions = torch.load('valid_reconstructions_nocommitment.pt')
+valid_originals = torch.load('valid_originals_nocommitment.pt')
+
+# Plot
+save_new(make_grid(valid_reconstructions.cpu().data + 0.5), 'make_grid_reconstruction_nocommitment.png')
+plt.show()
+
+save_new(make_grid(valid_originals.cpu() + 0.5), 'make_grid_original_nocommitment.png')
+plt.show()
+
+# %%
+## Had error with import umap.umap_ as umap
+import umap.umap_ as umap
+
+proj = umap.UMAP(n_neighbors=3,
+                 min_dist=0.1,
+                 metric='cosine').fit_transform(model._vq_vae._embedding.weight.data.cpu())
+
+np.save('umap_embeddings_nocommitment.npy', proj)
+
+# proj = np.load('umap_embeddings.npy')
+
+# # %%
+plt.scatter(proj[:,0], proj[:,1], alpha=0.3)
+plt.savefig('embedding_nocommitment.png')
+
+# # %%
+
+
